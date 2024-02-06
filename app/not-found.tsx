4117b9112eed90eca202efbb4a21c65b0da2e31c
Newsome Cyberpatriot <nhsjrotccyber@gmail.com>
2024-02-06 21:32:25 +0000

    real

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,18 @@
+"use client"
+
+import Router from "next/navigation"
+
+function handleClick() {
+    Router.redirect("/")
+}
+
 export default function NotFoundPage() {
     return(
         <div>
             <title>Not Found | Doge Clicker</title>
             <div className="flex flex-col w-full h-[88vh] bg-blue-600 items-center justify-center">
                 <h1 className="text-2xl">Error 404 - Could Not Find The Page You're Looking For :/</h1>
+                <button onClick={handleClick}>Home</button>
             </div>
         </div>
     )
