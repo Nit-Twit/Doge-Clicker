eeef1643ceec131917aed30de301ea26f45204eb
Ethan Belcher <157034077+Nit-Twit@users.noreply.github.com>
2024-07-18 22:37:12 +0000

    build 0.1a-hf1

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFoundPage() {
         <div>
             <title>Not Found | Doge Clicker</title>
             <div className="flex flex-col w-full h-[88vh] bg-blue-600 items-center justify-center gap-4">
-                <h1 className="text-2xl">Error 404 - Could Not Find The Page You're Looking For :/</h1>
+                <h1 className="text-2xl">Error 404 - Could Not Find The Page You&apos;re Looking For :/</h1>
                 <Link className="text-xl" href={"/"} >Home</Link>
             </div>
         </div>
